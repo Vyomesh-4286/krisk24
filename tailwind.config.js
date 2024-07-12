a950faca9235ceb48f23bee68b64eb5a7457c8f9
Vyomesh-4286 <vyomeshkumar.makadia@gmail.com>
2024-07-12 18:34:03 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Avenir: 'Avenir LT Std',
+        NeueHaas: 'NeueHaasUnicaW1G-Regular'
+      }
+    },
+  },
+  plugins: [],
+}
+

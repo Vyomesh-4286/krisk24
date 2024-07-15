08a4f9b4d59c1e43a4f76d182a023bb083a270ca
Vyomesh-4286 <vyomeshkumar.makadia@gmail.com>
2024-07-15 17:01:21 +0530

    Js Fixed

--- src/js/index.js
+++ src/js/index.js
@@ -676,34 +676,55 @@ $(document).ready(function () {
     // Function to calculate loading time
     function getLoadingTime() {
         const timing = window.performance.timing;
-        const loadTime = timing.loadEventEnd - timing.navigationStart;
-        return loadTime;
+        const loadEventEnd = timing.loadEventEnd;
+        const navigationStart = timing.navigationStart;
+
+        if (loadEventEnd > 0) {
+            return loadEventEnd - navigationStart;
+        } else {
+            console.warn('loadEventEnd is 0, returning a fallback value');
+            return new Date().getTime() - navigationStart;
+        }
     }
 
     // Function to display loading time in loader
     function displayLoadingTime() {
         const loader = document.getElementById('loader');
+        if (!loader) {
+            console.error('Loader element not found');
+            return;
+        }
+
         const loadTime = getLoadingTime();
+        console.log('Load time:', loadTime);
 
         // Set animation duration for 'dis' class elements
-        const disElements = document.querySelectorAll('.dis');
+        const disElements = document.querySelectorAll('#loader .dis');
         disElements.forEach(element => {
             element.style.animationDuration = loadTime + 'ms';
         });
+        console.log('Set animation duration for dis elements');
 
         // Set half of the loadTime for specific classes
         const halfLoadTime = loadTime / 2;
-        const classesToAdjust = ['.ic', '.yazi_k', '.yazi_risk', '.yazi_24'];
+        console.log('Half load time:', halfLoadTime);
+
+        const classesToAdjust = ['#loader .ic', '#loader .yazi_k', '#loader .yazi_risk', '#loader .yazi_24'];
 
         classesToAdjust.forEach(className => {
             const elements = document.querySelectorAll(className);
+            if (elements.length === 0) {
+                console.warn(`No elements found for class ${className}`);
+            }
             elements.forEach(element => {
                 element.style.animationDuration = halfLoadTime + 'ms';
             });
         });
+        console.log('Set animation duration for specific classes');
 
         // Hide the loader
         loader.style.display = 'none';
+        console.log('Loader hidden');
     }
 
     // Wait for the window to fully load before displaying the loading time
@@ -711,6 +732,7 @@ $(document).ready(function () {
         displayLoadingTime();
     });
 });
+
 
 $(document).ready(function () {
     // put the animation on load, otherwise it bugs out
@@ -940,7 +962,7 @@ $(document).ready(function () {
 
         const baseVertexShader = compileShader(gl.VERTEX_SHADER, `
       precision highp float;
-  
+
       attribute vec2 aPosition;
       varying vec2 vUv;
       varying vec2 vL;
@@ -948,7 +970,7 @@ $(document).ready(function () {
       varying vec2 vT;
       varying vec2 vB;
       uniform vec2 texelSize;
-  
+
       void main () {
           vUv = aPosition * 0.5 + 0.5;
           vL = vUv - vec2(texelSize.x, 0.0);
@@ -961,13 +983,13 @@ $(document).ready(function () {
 
         const blurVertexShader = compileShader(gl.VERTEX_SHADER, `
       precision highp float;
-  
+
       attribute vec2 aPosition;
       varying vec2 vUv;
       varying vec2 vL;
       varying vec2 vR;
       uniform vec2 texelSize;
-  
+
       void main () {
           vUv = aPosition * 0.5 + 0.5;
           float offset = 1.33333333;
@@ -980,12 +1002,12 @@ $(document).ready(function () {
         const blurShader = compileShader(gl.FRAGMENT_SHADER, `
       precision mediump float;
       precision mediump sampler2D;
-  
+
       varying vec2 vUv;
       varying vec2 vL;
       varying vec2 vR;
       uniform sampler2D uTexture;
-  
+
       void main () {
           vec4 sum = texture2D(uTexture, vUv) * 0.29411764;
           sum += texture2D(uTexture, vL) * 0.35294117;
@@ -997,10 +1019,10 @@ $(document).ready(function () {
         const copyShader = compileShader(gl.FRAGMENT_SHADER, `
       precision mediump float;
       precision mediump sampler2D;
-  
+
       varying highp vec2 vUv;
       uniform sampler2D uTexture;
-  
+
       void main () {
           gl_FragColor = texture2D(uTexture, vUv);
       }
@@ -1009,11 +1031,11 @@ $(document).ready(function () {
         const clearShader = compileShader(gl.FRAGMENT_SHADER, `
       precision mediump float;
       precision mediump sampler2D;
-  
+
       varying highp vec2 vUv;
       uniform sampler2D uTexture;
       uniform float value;
-  
+
       void main () {
           gl_FragColor = value * texture2D(uTexture, vUv);
       }
@@ -1021,9 +1043,9 @@ $(document).ready(function () {
 
         const colorShader = compileShader(gl.FRAGMENT_SHADER, `
       precision mediump float;
-  
+
       uniform vec4 color;
-  
+
       void main () {
           gl_FragColor = color;
       }
@@ -1033,7 +1055,7 @@ $(document).ready(function () {
         const displayShaderSource = `
       precision highp float;
       precision highp sampler2D;
-  
+
       varying vec2 vUv;
       varying vec2 vL;
       varying vec2 vR;
@@ -1043,31 +1065,31 @@ $(document).ready(function () {
       uniform sampler2D uDithering;
       uniform vec2 ditherScale;
       uniform vec2 texelSize;
-  
+
       vec3 linearToGamma (vec3 color) {
           color = max(color, vec3(0));
           return max(1.055 * pow(color, vec3(0.416666667)) - 0.055, vec3(0));
       }
-  
+
       void main () {
           vec3 c = texture2D(uTexture, vUv).rgb;
-  
+
       #ifdef SHADING
           vec3 lc = texture2D(uTexture, vL).rgb;
           vec3 rc = texture2D(uTexture, vR).rgb;
           vec3 tc = texture2D(uTexture, vT).rgb;
           vec3 bc = texture2D(uTexture, vB).rgb;
-  
+
           float dx = length(rc) - length(lc);
           float dy = length(tc) - length(bc);
-  
+
           vec3 n = normalize(vec3(dx, dy, length(texelSize)));
           vec3 l = vec3(0.0, 0.0, 1.0);
-  
+
           float diffuse = clamp(dot(n, l) + 0.7, 0.7, 1.0);
           c *= diffuse;
       #endif
-  
+
           float a = max(c.r, max(c.g, c.b));
           gl_FragColor = vec4(c, a);
       }
@@ -1076,14 +1098,14 @@ $(document).ready(function () {
         const splatShader = compileShader(gl.FRAGMENT_SHADER, `
       precision highp float;
       precision highp sampler2D;
-  
+
       varying vec2 vUv;
       uniform sampler2D uTarget;
       uniform float aspectRatio;
       uniform vec3 color;
       uniform vec2 point;
       uniform float radius;
-  
+
       void main () {
           vec2 p = vUv - point.xy;
           p.x *= aspectRatio;
@@ -1096,7 +1118,7 @@ $(document).ready(function () {
         const advectionShader = compileShader(gl.FRAGMENT_SHADER, `
       precision highp float;
       precision highp sampler2D;
-  
+
       varying vec2 vUv;
       uniform sampler2D uVelocity;
       uniform sampler2D uSource;
@@ -1104,21 +1126,21 @@ $(document).ready(function () {
       uniform vec2 dyeTexelSize;
       uniform float dt;
       uniform float dissipation;
-  
+
       vec4 bilerp (sampler2D sam, vec2 uv, vec2 tsize) {
           vec2 st = uv / tsize - 0.5;
-  
+
           vec2 iuv = floor(st);
           vec2 fuv = fract(st);
-  
+
           vec4 a = texture2D(sam, (iuv + vec2(0.5, 0.5)) * tsize);
           vec4 b = texture2D(sam, (iuv + vec2(1.5, 0.5)) * tsize);
           vec4 c = texture2D(sam, (iuv + vec2(0.5, 1.5)) * tsize);
           vec4 d = texture2D(sam, (iuv + vec2(1.5, 1.5)) * tsize);
-  
+
           return mix(mix(a, b, fuv.x), mix(c, d, fuv.x), fuv.y);
       }
-  
+
       void main () {
       #ifdef MANUAL_FILTERING
           vec2 coord = vUv - dt * bilerp(uVelocity, vUv, texelSize).xy * texelSize;
@@ -1136,26 +1158,26 @@ $(document).ready(function () {
         const divergenceShader = compileShader(gl.FRAGMENT_SHADER, `
       precision mediump float;
       precision mediump sampler2D;
-  
+
       varying highp vec2 vUv;
       varying highp vec2 vL;
       varying highp vec2 vR;
       varying highp vec2 vT;
       varying highp vec2 vB;
       uniform sampler2D uVelocity;
-  
+
       void main () {
           float L = texture2D(uVelocity, vL).x;
           float R = texture2D(uVelocity, vR).x;
           float T = texture2D(uVelocity, vT).y;
           float B = texture2D(uVelocity, vB).y;
-  
+
           vec2 C = texture2D(uVelocity, vUv).xy;
           if (vL.x < 0.0) { L = -C.x; }
           if (vR.x > 1.0) { R = -C.x; }
           if (vT.y > 1.0) { T = -C.y; }
           if (vB.y < 0.0) { B = -C.y; }
-  
+
           float div = 0.5 * (R - L + T - B);
           gl_FragColor = vec4(div, 0.0, 0.0, 1.0);
       }
@@ -1164,14 +1186,14 @@ $(document).ready(function () {
         const curlShader = compileShader(gl.FRAGMENT_SHADER, `
       precision mediump float;
       precision mediump sampler2D;
-  
+
       varying highp vec2 vUv;
       varying highp vec2 vL;
       varying highp vec2 vR;
       varying highp vec2 vT;
       varying highp vec2 vB;
       uniform sampler2D uVelocity;
-  
+
       void main () {
           float L = texture2D(uVelocity, vL).y;
           float R = texture2D(uVelocity, vR).y;
@@ -1185,7 +1207,7 @@ $(document).ready(function () {
         const vorticityShader = compileShader(gl.FRAGMENT_SHADER, `
       precision highp float;
       precision highp sampler2D;
-  
+
       varying vec2 vUv;
       varying vec2 vL;
       varying vec2 vR;
@@ -1195,19 +1217,19 @@ $(document).ready(function () {
       uniform sampler2D uCurl;
       uniform float curl;
       uniform float dt;
-  
+
       void main () {
           float L = texture2D(uCurl, vL).x;
           float R = texture2D(uCurl, vR).x;
           float T = texture2D(uCurl, vT).x;
           float B = texture2D(uCurl, vB).x;
           float C = texture2D(uCurl, vUv).x;
-  
+
           vec2 force = 0.5 * vec2(abs(T) - abs(B), abs(R) - abs(L));
           force /= length(force) + 0.0001;
           force *= curl * C;
           force.y *= -1.0;
-  
+
           vec2 velocity = texture2D(uVelocity, vUv).xy;
           velocity += force * dt;
           velocity = min(max(velocity, -1000.0), 1000.0);
@@ -1218,7 +1240,7 @@ $(document).ready(function () {
         const pressureShader = compileShader(gl.FRAGMENT_SHADER, `
       precision mediump float;
       precision mediump sampler2D;
-  
+
       varying highp vec2 vUv;
       varying highp vec2 vL;
       varying highp vec2 vR;
@@ -1226,7 +1248,7 @@ $(document).ready(function () {
       varying highp vec2 vB;
       uniform sampler2D uPressure;
       uniform sampler2D uDivergence;
-  
+
       void main () {
           float L = texture2D(uPressure, vL).x;
           float R = texture2D(uPressure, vR).x;
@@ -1242,7 +1264,7 @@ $(document).ready(function () {
         const gradientSubtractShader = compileShader(gl.FRAGMENT_SHADER, `
       precision mediump float;
       precision mediump sampler2D;
-  
+
       varying highp vec2 vUv;
       varying highp vec2 vL;
       varying highp vec2 vR;
@@ -1250,7 +1272,7 @@ $(document).ready(function () {
       varying highp vec2 vB;
       uniform sampler2D uPressure;
       uniform sampler2D uVelocity;
-  
+
       void main () {
           float L = texture2D(uPressure, vL).x;
           float R = texture2D(uPressure, vR).x;
